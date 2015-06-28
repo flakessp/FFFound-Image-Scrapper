6674dcecfcdcdf0c4a416a2816e461f921770bf2
Sergei Panfilov <flakessp@gmail.com>
2015-06-28 22:45:09 +0300

    Added HTTP get for checking status errors

--- server/server.js
+++ server/server.js
@@ -3,25 +3,37 @@ Meteor.startup(function () {
 
   Meteor.methods({
     getImages: function () {
-      result = Meteor.http.get("http://ffffound.com/?offset=25");
+      result = Meteor.http.get("http://ffffound.com/?offset=0");
       $ = cheerio.load(result.content);
 
       data = $(".description").html();
 
       var images = [];
       $('.description').each(function(i, elem) {
+        var now = new Date().getTime();
         images[i] = $(this).html().split('<br>')[0];
 
         if(images[i].substr(0,7) != 'http://') images[i] = 'http://' + images[i];
-        
-        console.log(images[i]);
+
+        var url = images[i];
+        HTTP.get(url, function (error, result) {
+          if (!error) {
+            console.log('Found a file!: ' + url);
+            console.log('Result: ' + result.statusCode);
+          } else {
+            console.log(error);
+            console.log('Error: ' + error);
+          };
+          });   
+
         // checking for uniqueness
         var postWithSameLink = Images.findOne({link: images[i]});
         if (postWithSameLink) return true;
         // end check
         Images.insert(
           {
-            link: images[i]
+            link: images[i],
+            submitted: now
           });
       });
       return images;
